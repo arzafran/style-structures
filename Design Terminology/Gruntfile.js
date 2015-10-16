f37cac5a8cf2faeb833516faab61f30e7577f53c
unknown <franco.arza@glb-d3521.globant.com>
2015-10-16 13:13:36 -0300

    Added compiled versions, and gruntfiles

---
+++ Design Terminology/Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      task: {
+        src: ['screen.less'],
+        dest: 'dist/result.css'
+      },
+      options: {
+        'paths': 'source',
+        'rootpath': '',
+        'compress': true,
+        'cleancss': true,
+        'ieCompat': true,
+        'optimization': null,
+        'strictImports': false,
+        'strictMath': false,
+        'strictUnits': false,
+        'syncimport': false,
+        'dumpLineNumbers': false,
+        'relativeUrls': false,
+        'customFunctions': null,
+        'report': 'min',
+        'sourceMap': false,
+        'sourceMapFilename': '',
+        'sourceMapURL': '',
+        'sourceMapBasepath': '',
+        'sourceMapRootpath': '',
+        'outputSourceFiles': false,
+        'modifyVars': null,
+        'banner': ''
+      }
+    },
+    
+    watch: {
+      files: ['*.less'],
+      tasks: ['build']
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['less']);
+};
